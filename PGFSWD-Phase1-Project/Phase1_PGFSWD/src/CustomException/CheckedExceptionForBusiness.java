9949ec94039a9cdbaa76574711a9cc7096ab15f7
Rupal Gupta <sriradharasbihariji@gmail.com>
2023-04-06 01:54:15 +0530

    SimpliLearnJavaProject

---
+++ PGFSWD-Phase1-Project/Phase1_PGFSWD/src/CustomException/CheckedExceptionForBusiness.java
@@ -0,0 +1,14 @@
+package CustomException;
+
+public class CheckedExceptionForBusiness extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CheckedExceptionForBusiness() {
+		super();	
+	}
+
+	public CheckedExceptionForBusiness(final String arg0) {
+		super(arg0);
+		}
+}
